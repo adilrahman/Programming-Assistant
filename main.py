9fb80b23fb50290c3db7cb2c0e64f7834f68b738
adilrahman <adilrahman20008@gmail.com>
2022-08-15 20:30:39 +0530

    updated project dir structures

--- main.py
+++ main.py
@@ -1,1 +1,67 @@
-print("something")
+from math import remainder
+from click import command
+from speech_recognition_engine import SpeechTextEngine
+import config
+from services.notion import NotionClient
+from datetime import datetime
+import time
+
+notion_integration_token = config.NOTION_INTEGRATION_TOKEN
+notion_database_id = config.NOTION_DATABASE_ID
+
+sr = SpeechTextEngine()
+notionClient = NotionClient(token=notion_integration_token,
+                            database_id=notion_database_id)
+
+if __name__ == "__main__":
+
+    remainder = 5
+    count = 0
+    while True:
+        count = 0
+        if sr.wakeup():
+            sr.speak("listening sir....")
+            time.sleep(0.5)
+            while True:
+                if count == remainder:
+                    sr.speak("you didn't said anything for a while sir")
+                    time.sleep(0.5)
+
+                command = sr.speech_recognition()
+                if "deactivate" in command: # for deactivate service temporarly
+                    print("Deactivate!")
+                    sr.speak("Deactivating")
+                    break
+
+                if "note" in command or "todo" in command: # for createing notes
+                    sr.speak("Tell me sir")
+                    note = sr.speech_recognition()
+
+                    if note == "":
+                        sr.speak("you didn't said anything")
+                        continue
+                    else:
+                        sr.speak('your todo is ' + note)
+                        sr.speak(" should i store?")
+
+                        command = sr.speech_recognition()
+                        if "no" in command:
+                            sr.speak("Ok sir")
+                            continue
+
+                    time_now = datetime.now().astimezone().isoformat()
+                    status = "Active"
+
+                    res = notionClient.create_page(desc=note,
+                                                   date=time_now,
+                                                   status=status)
+                    if res.status_code == 200:
+                        print("created successfully....!!!")
+                        sr.speak("new note created")
+                    else:
+                        print(f"error :-> status code = {res.status_code}")
+                        sr.speak("can't store the note")
+
+                if "exit" in command: # for exiting program
+                    sr.speak("program terminating")
+                    exit()
---
+++ speech_recognition_engine/__init__.py
@@ -0,0 +1,1 @@
+from .speech_recognition_engine import SpeechTextEngine
