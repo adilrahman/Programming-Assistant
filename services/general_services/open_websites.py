21b44f33ae392f3b9e50745b5f903c537eaca4d7
adilrahman <adilrahman20008@gmail.com>
2022-08-16 20:35:03 +0530

    added search using prompt function

--- services/general_services/open_websites.py
+++ services/general_services/open_websites.py
@@ -55,8 +55,44 @@ class WebsiteController:
                 break
             wb.open(sol)
 
+    @staticmethod
+    def search_prompt():
+        '''
+        description:
+            it will open `top` #nums_results `in web browser`
+
+        inputs:
+           open a prompt to enter the queries
+
+        output:
+            open the results in web browser\n
+            return None
+        '''
+        query = pyautogui.prompt(title="Enter the query?")
+
+        if len(query) == 0:
+            return False
+
+        nums_results = pyautogui.prompt(
+            title="how many results you want?")
+
+        if len(nums_results) == 0:
+            return False
+
+        nums_results = int(nums_results)
+
+        solutions = search(query)
+        for idx, sol in enumerate(solutions):
+            if idx == nums_results:
+                break
+            wb.open(sol)
+
+    def search_query(self, query: str):
+        pass
+
 
 if __name__ == "__main__":
-    web_contr = WebsiteController()
-    web_contr.open_website("twitter")
-    print("s")
+    # web_contr = WebsiteController()
+    # web_contr.open_website("twitter")
+    # print("s")
+    WebsiteController.search_prompt()
